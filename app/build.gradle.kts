d15437cda3f052914c67b502853852efdd99de76
mteja <matthew.teja@gmail.com>
2024-09-22 23:00:04 -0700

    added nav bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.databinding.runtime)
+    implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/project/HomeActivity.kt
+++ app/src/main/java/com/project/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.project
 
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.project.base.BaseActivity
 
 class HomeActivity : BaseActivity() {
@@ -15,9 +17,13 @@ class HomeActivity : BaseActivity() {
             val fragment = HomeFragment()
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.main_content, fragment)
+                .add(R.id.activity_home_main_content, fragment)
                 .commit()
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.activity_home_main_content) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.activity_home_bottom_navigation).setupWithNavController(navController)
 
     }
 }
