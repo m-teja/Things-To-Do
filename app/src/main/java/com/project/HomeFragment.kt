6920a52d53c7c01f20268f301570aaa090bacb7c
mteja <matthew.teja@gmail.com>
2024-10-02 22:57:05 -0700

    added random button

--- app/src/main/java/com/project/HomeFragment.kt
+++ app/src/main/java/com/project/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.project
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.project.adapter.HomeAdapter
@@ -8,6 +10,7 @@ import com.project.base.BaseFragment
 
 
 class HomeFragment : BaseFragment(R.layout.fragment_home) {
+    private val dataSet = arrayListOf("things", "to", "do")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +20,19 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val dataSet = arrayListOf("things", "to", "do")
         val homeAdapter = HomeAdapter(dataSet)
 
         val recyclerView: RecyclerView = view.findViewById(R.id.home_list)
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = homeAdapter
+
+        view.findViewById<Button>(R.id.home_random_button).setOnClickListener {
+            Toast.makeText(context, getRandomSuggestion(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getRandomSuggestion(): String {
+        return dataSet[(Math.random() * (dataSet.size)).toInt()]
     }
 
 }
