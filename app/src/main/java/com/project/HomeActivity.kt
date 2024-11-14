054a6b18789822530087861196d7c6c9a33af3be
mteja <matthew.teja@gmail.com>
2024-11-13 21:00:20 -0800

    changed home activity to compose

--- app/src/main/java/com/project/HomeActivity.kt
+++ app/src/main/java/com/project/HomeActivity.kt
@@ -1,38 +1,87 @@
 package com.project
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.google.android.material.navigation.NavigationBarView
 import com.project.base.BaseActivity
-import com.project.base.BaseFragment
 
 class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        val homeFragment = HomeFragment()
-        val mapFragment = MapFragment()
-        val settingsFragment = SettingsFragment()
-
-        findViewById<NavigationBarView>(R.id.activity_home_bottom_navigation).setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> setCurrentFragment(homeFragment)
-                R.id.map -> setCurrentFragment(mapFragment)
-                R.id.settings -> setCurrentFragment(settingsFragment)
-            }
-            true
+        setContent {
+            screen()
         }
 
-        setCurrentFragment(homeFragment)
     }
 
+    @Preview
+    @Composable
+    private fun screenPreview() {
+        screen()
+    }
 
-    private fun setCurrentFragment(fragment: BaseFragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.activity_home_main_content, fragment)
-            commit()
+    @Composable
+    private fun screen() {
+        Scaffold (
+            bottomBar = {
+                navigationBar()
+            }
+        ) { content ->
+            Text(
+                modifier = Modifier.padding(content),
+                text = "test"
+            )
+        }
+    }
+
+    @Composable
+    private fun navigationBar() {
+        NavigationBar (
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.primary,
+            content = {
+                NavigationBarItem(
+                    label = { Text(text = "Home") },
+                    icon = { Icon(Icons.Filled.Home, "home") },
+                    selected = false,
+                    onClick = {
+
+                    },
+                    colors = NavigationBarItemDefaults.colors()
+                )
+                NavigationBarItem(
+                    label = { Text(text = "Map") },
+                    icon = { Icon(Icons.Filled.LocationOn, "map") },
+                    selected = false,
+                    onClick = {},
+                    colors = NavigationBarItemDefaults.colors()
+                )
+                NavigationBarItem(
+                    label = { Text(text = "Settings") },
+                    icon = { Icon(Icons.Filled.Settings, "settings") },
+                    selected = false,
+                    onClick = {},
+                    colors = NavigationBarItemDefaults.colors()
+                )
+            },
+        )
     }
 }
