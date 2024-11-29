cc9d5b51806335ce848adde6e761ad70b19a3686
mteja <matthew.teja@gmail.com>
2024-11-28 19:44:14 -0800

    made ui for home screen

--- app/src/main/java/com/thingstodo/screens/HomeScreen.kt
+++ app/src/main/java/com/thingstodo/screens/HomeScreen.kt
@@ -1,25 +1,45 @@
 package com.thingstodo.screens
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.thingstodo.R
 import com.thingstodo.data.Activity
 import com.thingstodo.data.Category
 import com.thingstodo.model.HomeViewModel
@@ -51,9 +71,9 @@ fun OptionList(
     optionItems: List<OptionItem>, onNavigateToMapScreen: (String, Int) -> Unit
 ) {
     LazyColumn (
-        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-    ){
+        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp),
+    ) {
         items(optionItems) { optionItem ->
             Option(optionItem, onNavigateToMapScreen)
         }
@@ -66,36 +86,87 @@ fun Option(
     onNavigateToMapScreen: (String, Int) -> Unit
 ) {
     Card (
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(4.dp),
         onClick = {
             onNavigateToMapScreen(optionItem.activity, 10)
         }
     ) {
-        Row (
+        Column (
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 20.dp),
-            horizontalArrangement = Arrangement.Center
+                .padding(horizontal = 20.dp, vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(25.dp)
         ) {
-            Text(
+            Row (
                 modifier = Modifier
-                    .fillMaxWidth(0.40f)
-                    .padding(start = 10.dp),
-                text = optionItem.activity
-            )
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Absolute.SpaceBetween
+            ) {
+                Column (
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ){
+                    Text(
+                        fontFamily = FontFamily.Serif,
+                        text = optionItem.activity
+                    )
 
-            Text(
-                modifier = Modifier.fillMaxWidth(0.40f),
-                text = optionItem.category
-            )
+                    Text(
+                        fontFamily = FontFamily.Serif,
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.secondary,
+                        text = optionItem.category
+                    )
+                }
 
-            optionItem.icon?.let {
-                Icon(
-                    modifier = Modifier.fillMaxWidth(),
-                    imageVector = it,
-                    contentDescription = optionItem.activity
-                )
+                optionItem.icon?.let {
+                    Icon(
+                        imageVector = it,
+                        contentDescription = optionItem.activity
+                    )
+                }
+            }
+            val middleWord = when (optionItem.activity[0].lowercaseChar()) {
+                'a', 'e', 'i', 'o', 'u' -> "an"
+                else -> "a"
+            }
+            val message = "Search for " + middleWord + " " + optionItem.activity
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Absolute.SpaceBetween
+            ) {
+
+                IconButton (
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .background(color = MaterialTheme.colorScheme.errorContainer)
+                            .padding(3.dp),
+                        imageVector = ImageVector.vectorResource(R.drawable.delete_icon),
+                        contentDescription = "delete"
+                    )
+                }
+
+                Row {
+                    Text(
+                        fontFamily = FontFamily.Serif,
+                        fontSize = 14.sp,
+                        text =  message
+                    )
+
+                    Icon (
+                        imageVector = ImageVector.vectorResource(R.drawable.link_search_icon),
+                        contentDescription = "link search"
+                    )
+                }
             }
         }
     }
