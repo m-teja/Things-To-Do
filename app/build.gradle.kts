dd189cccfb06c72f6b1ea6839fe129229d5027c2
mteja <matthew.teja@gmail.com>
2024-12-09 22:12:05 -0800

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.teja_app_productions_things_to_do"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
